2ef358c719ac55849e466a86ac8b27ae931bf835
Mihai Smarandache <mihai.sanfran@gmail.com>
2020-05-18 12:38:02 -0700

    Display Top 20 React news

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component, useState, useEffect } from 'react';
 
-function App() {
+const App = () => {
+  // create state with empty news array
+  const [ news, setNews ] = useState([]);
+
+  const fetchNews = () => {
+    fetch('http://hn.algolia.com/api/v1/search?query=react')
+      .then(result => result.json())
+      .then(data => setNews(data.hits))
+      .catch(error => console.log(error));
+  };
+
+  // useEffect runs when component mounts and when there is a change in state
+  useEffect(() => {
+    fetchNews()
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>News</h2>
+      {news.map((item, index) => (<p key={index}>{item.title}</p>))}
     </div>
-  );
+  )
 }
 
 export default App;
