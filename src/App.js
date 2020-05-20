c3c9542b5071c9aaace417acbaf48828c500e187
Mihai Smarandache <mihai.sanfran@gmail.com>
2020-05-20 08:52:17 -0700

    Load data on button click

--- src/App.js
+++ src/App.js
@@ -4,9 +4,10 @@ const App = () => {
   // create state with empty news array
   const [ news, setNews ] = useState([]);
   const [ searchQuery, setSearchQuery ] = useState('react');
+  const [ url, setUrl ] = useState('http://hn.algolia.com/api/v1/search?query=react');
 
   const fetchNews = () => {
-    fetch(`http://hn.algolia.com/api/v1/search?query=${searchQuery}`)
+    fetch(url)
       .then(result => result.json())
       .then(data => setNews(data.hits))
       .catch(error => console.log(error));
@@ -15,16 +16,23 @@ const App = () => {
   // useEffect runs when component mounts and when there is a change in state
   useEffect(() => {
     fetchNews()
-  }, [searchQuery]); // run useEffect only when searchQuery changes
+  }, [url]); // run useEffect only when url changes (when btn is clicked)
 
-  const handleChange = (e) => {
+  const handleChange = e => {
     setSearchQuery(e.target.value)
+  }
+
+  const handleSubmit = e => {
+    // prevent page from refreshing on submit
+    e.preventDefault()
+
+    setUrl(`http://hn.algolia.com/api/v1/search?query=${searchQuery}`);
   }
 
   return (
     <div>
       <h2>News</h2>
-      <form>
+      <form onSubmit={handleSubmit}>
         <input type="text" value={searchQuery} onChange={handleChange} />
         <button>Search</button>
       </form>
