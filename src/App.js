00c61c206ddb2bc250102b22c51799aee228b562
Mihai Smarandache <mihai.sanfran@gmail.com>
2020-05-19 21:50:05 -0700

    Add search form

--- src/App.js
+++ src/App.js
@@ -3,9 +3,10 @@ import React, { Component, useState, useEffect } from 'react';
 const App = () => {
   // create state with empty news array
   const [ news, setNews ] = useState([]);
+  const [ searchQuery, setSearchQuery ] = useState('react');
 
   const fetchNews = () => {
-    fetch('http://hn.algolia.com/api/v1/search?query=react')
+    fetch(`http://hn.algolia.com/api/v1/search?query=${searchQuery}`)
       .then(result => result.json())
       .then(data => setNews(data.hits))
       .catch(error => console.log(error));
@@ -14,11 +15,19 @@ const App = () => {
   // useEffect runs when component mounts and when there is a change in state
   useEffect(() => {
     fetchNews()
-  });
+  }, [searchQuery]); // run useEffect only when searchQuery changes
+
+  const handleChange = (e) => {
+    setSearchQuery(e.target.value)
+  }
 
   return (
     <div>
       <h2>News</h2>
+      <form>
+        <input type="text" value={searchQuery} onChange={handleChange} />
+        <button>Search</button>
+      </form>
       {news.map((item, index) => (<p key={index}>{item.title}</p>))}
     </div>
   )
